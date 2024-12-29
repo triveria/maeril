5cd813a24d3c5f4b95969ea7ac43ae8cd3e6fd44
Michael Wagner <michael.wagner.31337@gmail.com>
2024-12-29 17:35:00 +0100

    prompt: Generate filename of processed prompt.md

--- src/maeril/prompt.py
+++ src/maeril/prompt.py
@@ -9,9 +9,9 @@ def replace_codefile(match):
     file_content = filepath.read_text()
     lines = file_content.splitlines()
 
-    content_with_line_numbers = ""
-    for line_number, line in enumerate(lines, start=1):
-        content_with_line_numbers += f"{line_number:3}: {line}\n"
+    content = ""
+    for line in lines:
+        content += f"{line}\n"
 
     # Extract the file extension for setting markdown hint
     md_hint = filepath.suffix.lower().lstrip(".") if filepath.suffix else 'text'
@@ -19,9 +19,8 @@ def replace_codefile(match):
     formatted_code_block = (
         f"## {filepath}\n"
         f"\n"
-        f"(With line numbers as a guide)\n"
         f"```{md_hint}\n"
-        f"{content_with_line_numbers}"
+        f"{content}"
         f"```\n"
     )
 
@@ -42,28 +41,27 @@ def replace_command(match):
 
 
 def main(input_path):
+    input_path = Path(input_path)
     command_pattern = re.compile(r'\[command:\s*(.+?)\]')
     codefile_pattern = re.compile(r'\[codefile:\s*(.+?)\]')
     comment_pattern = re.compile(r'<!--.*-->')
 
-    input_md = Path(input_path).read_text()
+    input_md = input_path.read_text()
     input_md = input_md.rstrip()  # Remove trailing whitespace and newlines
 
-    output_lines = []
+    md_content = ""
     for line in input_md.splitlines():
         if comment_pattern.search(line):
             continue  # Skip lines containing comments
         line = command_pattern.sub(replace_command, line)
         line = codefile_pattern.sub(replace_codefile, line)
-        output_lines.append(line)
-
-    converted_md_content = ""
-    for line in output_lines:
-        converted_md_content += line + "\n"
+        md_content += f"{line}\n"
 
     # Ensure the content ends with exactly one newline
-    converted_md_content = converted_md_content.rstrip('\n') + '\n'
+    md_content = md_content.rstrip('\n') + '\n'
 
-    output_path = "output.md"
-    Path(output_path).write_text(converted_md_content)
+    output_filename = f"{input_path.stem}.generated{input_path.suffix}"
+    output_path = input_path.with_name(output_filename)
+
+    Path(output_path).write_text(md_content)
     print(f"Converted {input_path} to {output_path}")
