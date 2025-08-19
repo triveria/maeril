40eb645091ead2c278649807b451da31079897e8
Michael Wagner <michael.wagner.31337@gmail.com>
2025-08-19 11:51:23 +0200

    Update devcontainer setup

--- src/maeril/dump_files/devcontainer_debian/default_zsh_setup.zsh
+++ src/maeril/dump_files/devcontainer_debian/default_zsh_setup.zsh
@@ -7,3 +7,23 @@ _maeril_completion() {
   eval $(env _TYPER_COMPLETE_ARGS="${words[1,$CURRENT]}" _MAERIL_COMPLETE=complete_zsh maeril)
 }
 compdef _maeril_completion maeril
+
+# ------------------------------------------------------------------------------
+# Custom Keybinding: Rebind Ctrl+L to run `clear`
+#
+# This creates a ZLE widget to clear the screen using the external `clear`
+# command, which also clears scrollback, and then redraws the prompt.
+# It is recommended to place this at the end of the .zshrc file.
+# ------------------------------------------------------------------------------
+function custom_clear_screen_widget() {
+    # Execute the external `clear` program
+    command clear
+    # Redraw the prompt and any text that was on the command line
+    zle redisplay
+}
+
+# Register the function as a new ZLE widget
+zle -N custom_clear_screen_widget
+
+# Bind the Ctrl+L key sequence to the new custom widget
+bindkey '^L' custom_clear_screen_widget
